31aa3832178a27c80131ca974ea70f098417aaaa
David Irén <id17din@cs.umu.se>
2024-10-07 17:29:01 +0200

    2:05:00

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2'>
-              <Image src={profilePic} alt="David Irén" className='w-full h-auto'/>
+              <Image src={profilePic} alt="David Irén" className='w-2/3 h-auto'/>
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality." className='!text-6xl !text-left'/>
