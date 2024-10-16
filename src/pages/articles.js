af9b023335eaca7d9fabc730321f079c0bc33714
David Irén <id17din@cs.umu.se>
2024-10-16 16:40:22 +0200

    4:59:25

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -67,7 +67,12 @@ const FeaturedArticle = ({img, title, time, summary, link}) => {
         <li className='col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl relative dark:bg-dark dark:border-light'>
             <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light' />
             <Link href={link} target='_blank' className='w-full cursor-pointer overflow-hidden rounded-lg inline-block'>
-                <FramerImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}} transition={{duration:0.2}}/>
+                <FramerImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}} transition={{duration:0.2}}
+                priority="true"
+                sizes='(max-width: 768px) 100vw,
+                       (max-width: 1200px) 50vw,
+                       50vm'
+                />
             </Link>
             <Link href={link} target='_blank'>
                 <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline'>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -18,7 +18,12 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
             <div className='absolute top-0 -right-3 -z-10 w-[100%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl' />
             <Link href={link} target='_blank' 
             className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
-                <FramerImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}} transition={{duration:0.2}}/>
+                <FramerImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}} transition={{duration:0.2}}
+                priority="true"
+                sizes='(max-width: 768px) 100vw,
+                       (max-width: 1200px) 50vw,
+                       50vm'
+                />
             </Link>
 
             <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
